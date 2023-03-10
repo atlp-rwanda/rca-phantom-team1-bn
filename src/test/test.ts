9f47369bd5554b5aeab74a56285c32ca5d7a06fa
Clarance Liberiste Ntwari <liberintwari@gmail.com>
2023-03-09 20:16:29 -0500

    chore: eslint and prettier configurations

--- src/test/test.ts
+++ src/test/test.ts
@@ -1,20 +1,19 @@
-import * as chai from 'chai';
-import * as sinon from 'sinon';
-import { describe, it } from 'mocha';
+import * as chai from 'chai'
+import * as sinon from 'sinon'
+import { describe, it } from 'mocha'
 
-const expect = chai.expect;
+const expect = chai.expect
 
 describe('Example Test Suite', () => {
-  it('should pass', () => {
-    expect(1 + 1).to.equal(2);
-  });
+    it('should pass', () => {
+        expect(1 + 1).to.equal(2)
+    })
 
-  it('should use a spy', () => {
-    const callback = sinon.spy();
+    it('should use a spy', () => {
+        const callback = sinon.spy()
 
-    callback('hello', 'world');
+        callback('hello', 'world')
 
-    expect(callback.calledOnce).to.be.true;
     expect(callback.calledWith('hello', 'world')).to.be.true;
   });
 });
