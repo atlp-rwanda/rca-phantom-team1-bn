56c3327cc154cfdbab2a0ab2dea321dce6a1ceaa
Clarance Liberiste Ntwari <liberintwari@gmail.com>
2023-03-13 10:10:15 -0400

    chore: eslint and prettier configs

--- src/test/test.js
+++ src/test/test.js
@@ -1,16 +1,16 @@
-const { expect } = require('chai');
-const sinon = require('sinon');
+const { expect } = require("chai");
+const sinon = require("sinon");
 
-describe('Example Test Suite', () => {
-    it('should pass', () => {
-        expect(1 + 1).to.equal(2)
-    })
+describe("Example Test Suite", () => {
+  it("should pass", () => {
+    expect(1 + 1).to.equal(2);
+  });
 
-    it('should use a spy', () => {
-        const callback = sinon.spy()
+  it("should use a spy", () => {
+    const callback = sinon.spy();
 
-        callback('hello', 'world')
+    callback("hello", "world");
 
-    expect(callback.calledWith('hello', 'world')).to.be.true;
+    expect(callback.calledWith("hello", "world")).to.be.true;
   });
 });
