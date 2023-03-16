114f834047babf8af30f1d2a72d8bf98a3f60fb3
IrakozeLoraine <53254885+IrakozeLoraine@users.noreply.github.com>
2023-03-16 10:02:53 +0200

    Chore/support multiple languages (#8)
    * chore: setup internationalization (multiple language support)
    
    * Merge branch 'develop' of https://github.com/atlp-rwanda/rca-phantom-team1-bn into chore/support-multiple-languages
    
    * chore: create separate files for multiple language support
    
    * chore: testing multiple language support

--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ const low = require("lowdb");
 const swaggerUI = require("swagger-ui-express");
 const swaggerJsDoc = require("swagger-jsdoc");
 const busesRouter = require("./api/routes/buses.routes");
+const locales = require('./config/languages');
 const { PORT } = require("./config/dotenv");
 
 const db = require("./db/models/index");
@@ -14,7 +15,7 @@ const db = require("./db/models/index");
 db.sequelize
   .sync()
   .then(() => {
-    console.log("Synced db.");
+    console.log('Synced db.');
   })
   .catch((err) => {
     console.log(`Failed to sync db: ${err.message}`);
@@ -41,6 +42,11 @@ const specs = swaggerJsDoc(options);
 
 const app = express();
 app.get("/", (_, res) => res.json({ message: "Welcome to Phantom API" }));
+
+//testing multiple language support
+app.get('/welcome', (_, res) =>
+  res.send({ english: locales('home', 'en'), kinyarwanda: locales('home') })
+);
 
 app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(specs));
 
