c60efa49743a76c2151f8dc3041ab1510ceef8b8
Clarance Liberiste Ntwari <liberintwari@gmail.com>
2023-03-20 09:02:03 -0400

    chore: database url config

--- src/db/config/config.js
+++ src/db/config/config.js
@@ -4,6 +4,7 @@ dotenv.config();
 
 export default {
   development: {
+    url: process.env.DATABASE_URL,
     username: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
