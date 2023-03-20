e8c8c1e6f4d2ca84b061e881c7c145b4d12c384f
Patrick Niyogitare <54368821+PatrickNiyogitare28@users.noreply.github.com>
2023-03-20 14:47:40 +0100

    Fix es6 incompatible db scripts (#11)
    * chore: rebase develop
    
    * bug(db-scripts): fix db scripts failing with es6

--- src/db/config/config.js
+++ src/db/config/config.js
@@ -1,13 +1,11 @@
-import dotenv from "dotenv";
+require("dotenv").config();
 
-dotenv.config();
-
-export default {
+module.exports = {
   development: {
     username: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
-    host: "127.0.0.1",
+    host: process.env.DB_HOST || "127.0.0.1",
     port: 5432,
     dialect: "postgres",
     dialectOptions: {
@@ -18,7 +16,7 @@ export default {
     username: process.env.DB_TEST_USERNAME,
     password: process.env.DB_TEST_PASSWORD,
     database: process.env.DB_TEST_NAME,
-    host: "127.0.0.1",
+    host: process.env.DB_TEST_HOST || "127.0.0.1",
     port: 5432,
     dialect: "postgres",
     dialectOptions: {
