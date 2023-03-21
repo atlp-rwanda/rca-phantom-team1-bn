e1e506cf6fe7d4fd89880e98a0c49523ee5b9016
Patrick Niyogitare <patrickniyogitare28@gmail.com>
2023-03-21 02:03:22 +0200

    fix: ci-cd

--- src/db/models/index.js
+++ src/db/models/index.js
@@ -7,6 +7,8 @@ import envConfigs from "../config/config.js";
 const basename = path.basename(__filename);
 const env = process.env.NODE_ENV || "development";
 const config = envConfigs[env];
+console.log("configs: .....");
+console.log(config);
 const db = {};
 
 let sequelize;
