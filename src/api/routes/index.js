ca5adf1ead558e95f8891adb4b85bdb53a6557f8
SauveJeanLuc <sauvejeanluc3@gmail.com>
2023-03-24 14:14:55 +0200

    add auth to appRouter

--- src/api/routes/index.js
+++ src/api/routes/index.js
@@ -1,11 +1,14 @@
 import { Router } from "express";
 import busesRouter from "./buses.routes";
+import authRouter from "./auth.routes";
 import roleRouter from "./roles.routes";
 import {
   checkUserLoggedIn,
   restrictTo,
 } from "../middlewares/protect.middleware";
 import ERoles from "../enums/ERole";
+import { validateLogin } from "../validations/auth.validator";
+import { checkUserExists } from "../middlewares/auth.middleware";
 
 const appRouter = Router();
 
@@ -16,5 +19,5 @@ appRouter.use(
   restrictTo(ERoles.ADMINISTRATOR),
   roleRouter
 );
-
+appRouter.use("/auth", validateLogin, checkUserExists, authRouter);
 export default appRouter;
