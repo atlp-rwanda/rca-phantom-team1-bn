55a41379925a4275ba563c5955fffcf1583a5cec
IrakozeLoraine <mukezwa@gmail.com>
2023-03-27 14:27:16 +0200

    feat:  add bus seeders

--- src/db/seeders/20230323144339-Bus.js
+++ src/db/seeders/20230323144339-Bus.js
@@ -3,23 +3,33 @@
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up (queryInterface, Sequelize) {
-    /**
-     * Add seed commands here.
-     *
-     * Example:
-     * await queryInterface.bulkInsert('People', [{
-     *   name: 'John Doe',
-     *   isBetaMember: false
-     * }], {});
-    */
+      await queryInterface.bulkInsert('Buses', [{
+        "id": 2,
+        "plate_number": "KL3MS",
+        "agency_id": 12321,
+        "driver_id": null,
+        "createdAt": "2023-03-27T12:00:50.450Z",
+        "updatedAt": "2023-03-27T12:00:50.450Z"
+      },
+      {
+        "id": 3,
+        "plate_number": "KL3MS",
+        "agency_id": 12321,
+        "driver_id": null,
+        "createdAt": "2023-03-27T12:02:24.050Z",
+        "updatedAt": "2023-03-27T12:02:24.050Z"
+      },
+      {
+        "id": 4,
+        "plate_number": "KL2AS",
+        "agency_id": 12321,
+        "driver_id": 12324,
+        "createdAt": "2023-03-27T12:22:11.119Z",
+        "updatedAt": "2023-03-27T12:22:11.119Z"
+      }])
   },
 
   async down (queryInterface, Sequelize) {
-    /**
-     * Add commands to revert seed here.
-     *
-     * Example:
-     * await queryInterface.bulkDelete('People', null, {});
-     */
+    await queryInterface.bulkDelete("roles", null, {});
   }
 };
