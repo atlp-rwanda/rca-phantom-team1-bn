316087249bdb9185e95441f45f8b9d7629a1548f
SAUVÉ Jean-Luc <68426990+SauveJeanLuc@users.noreply.github.com>
2023-03-28 15:48:27 +0200

    chore: Log In (#13)
    * chore: rebase develop
    
    * bug(db-scripts): fix db scripts failing with es6
    
    * feat: add login functionality
    
    * refine signin operation
    
    * added tests for login
    
    * refine auth tests
    
    * fix dotenv - linting
    
    * create env file while running tests
    
    * add auth to appRouter
    
    * refine sign-in tests V.0.1
    
    * test CI/CD v.0.1
    
    * test CI/CD v.0.2
    
    * test CI/CD v.0.3
    
    * test CI/CD v.0.4
    
    * add alternative values to secrets
    
    ---------
    
    Co-authored-by: Patrick Niyogitare <patrickniyogitare28@gmail.com>

--- src/db/seeders/20230316084035-roles.js
+++ src/db/seeders/20230316084035-roles.js
@@ -3,61 +3,66 @@
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface, Sequelize) {
-    await queryInterface.bulkInsert(
-      "roles",
-      [
-        {
-          title: "driver",
-          description:
-            "Control the bus movement i.e. start, stop, and change bus speed. View the bus movement. Edit their profile",
-          privileges: [
-            "control_bus_movement",
-            "view_bus_movement",
-            "edit_profile",
-          ],
-          createdAt: new Date(),
-          updatedAt: new Date(),
-        },
-        {
-          title: "operator",
-          description:
-            "Manage (create, update, delete) routes. Manage (create, update, delete) buses. Manage (create, update, delete) bus to route assignment. Manage (create, update, delete) driver to bus assignment. View the bus movement. Edit their profile",
-          privileges: [
-            "manage_routes",
-            "manage_buses",
-            "manage_bus_route_assignment",
-            "manage_driver_bus_assignment",
-            "view_bus_movement",
-            "edit_profile",
-          ],
-          createdAt: new Date(),
-          updatedAt: new Date(),
-        },
-        {
-          title: "administrator",
-          description:
-            "Register and remove both drivers & operators. Manage (create, update, delete) routes. Manage (create, update, delete) buses. Manage (create, update, delete) bus to route assignment. Manage (create, update, delete) driver to bus assignment. View the bus movement",
-          privileges: [
-            "register_drivers_operators",
-            "manage_routes",
-            "manage_buses",
-            "manage_bus_route_assignment",
-            "manage_driver_bus_assignment",
-            "view_bus_movement",
-          ],
-          createdAt: new Date(),
-          updatedAt: new Date(),
-        },
-        {
-          title: "passenger",
-          description: "View the bus movement, routes and vehicles",
-          privileges: ["view_bus_movements", "views_routes", "views_vehicles"],
-          createdAt: new Date(),
-          updatedAt: new Date(),
-        },
-      ],
-      {}
+    const existingRoles = await queryInterface.sequelize.query(
+      "SELECT * FROM roles"
     );
+    if (existingRoles[0].length === 0) {
+      await queryInterface.bulkInsert(
+        "roles",
+        [
+          {
+            title: "driver",
+            description:
+              "Control the bus movement i.e. start, stop, and change bus speed. View the bus movement. Edit their profile",
+            privileges: [
+              "control_bus_movement",
+              "view_bus_movement",
+              "edit_profile",
+            ],
+            createdAt: new Date(),
+            updatedAt: new Date(),
+          },
+          {
+            title: "operator",
+            description:
+              "Manage (create, update, delete) routes. Manage (create, update, delete) buses. Manage (create, update, delete) bus to route assignment. Manage (create, update, delete) driver to bus assignment. View the bus movement. Edit their profile",
+            privileges: [
+              "manage_routes",
+              "manage_buses",
+              "manage_bus_route_assignment",
+              "manage_driver_bus_assignment",
+              "view_bus_movement",
+              "edit_profile",
+            ],
+            createdAt: new Date(),
+            updatedAt: new Date(),
+          },
+          {
+            title: "administrator",
+            description:
+              "Register and remove both drivers & operators. Manage (create, update, delete) routes. Manage (create, update, delete) buses. Manage (create, update, delete) bus to route assignment. Manage (create, update, delete) driver to bus assignment. View the bus movement",
+            privileges: [
+              "register_drivers_operators",
+              "manage_routes",
+              "manage_buses",
+              "manage_bus_route_assignment",
+              "manage_driver_bus_assignment",
+              "view_bus_movement",
+            ],
+            createdAt: new Date(),
+            updatedAt: new Date(),
+          },
+          {
+            title: "passenger",
+            description: "View the bus movement, routes and vehicles",
+            privileges: ["view_bus_movements", "views_routes", "views_vehicles"],
+            createdAt: new Date(),
+            updatedAt: new Date(),
+          },
+        ],
+        {}
+      );
+    }
   },
 
   async down(queryInterface, Sequelize) {
