68f5eb2116cfa4b045f629b922f4526329cc7366
Ntwari Egide <ntwariegide2@gmail.com>
2023-03-31 14:35:06 +0200

    fix: route controller methods

--- src/api/controllers/routes.controller.js
+++ src/api/controllers/routes.controller.js
@@ -1,32 +1,82 @@
-
+import { StatusCodes } from "http-status-codes";
 import { createRoute, deleteRoute, getAllRoutes, getRouteById, updateRoute } from "../services/route.service";
 
-export  const getRoutes = (req, res) => {
-    const routes = getAllRoutes;
-    
-    res.send(routes);
-}
+export const getRoutes = async (req, res, next) => {
+  try {
+    const routes = await getAllRoutes();
+    res.status(StatusCodes.OK).json({ success: true, data: routes });
+  } catch (error) {
+    next(error);
+  }
+};
 
-export const getRoute = (req, res) => {
-    const route = getRouteById(req.params.id);
-    
-    res.send(route);
-}
+export const getRoute = async (req, res, next) => {
+  const { id } = req.params;
+  try {
+    const route = await getRouteById(id);
+    if (!route) {
+      return res.status(StatusCodes.NOT_FOUND).json({
+        success: false,
+        message: `Route with id [${id}] not found`,
+      });
+    }
+    res.status(StatusCodes.OK).json({ success: true, data: route });
+  } catch (error) {
+    next(error);
+  }
+};
 
-export const saveRoute = (req, res) => {
-    const route = createRoute(req.body);
-    
-    res.send(route);
-}
+export const saveRoute = async (req, res, next) => {
+  const { body } = req;
+  try {
+    const route = await createRoute(body);
+    res.status(StatusCodes.CREATED).json({
+      success: true,
+      message: "Route created successfully",
+      data: route,
+    });
+  } catch (error) {
+    next(error);
+  }
+};
 
-export const routeUpdate = (req, res) => {
-    const route = updateRoute(req.params.id, req.body);
-    
-    res.send(route);
-}
+export const routeUpdate = async (req, res, next) => {
+  const { id } = req.params;
+  const { body } = req;
+  try {
+    const updatedRoute = await updateRoute(id, body);
+    if (!updatedRoute) {
+      return res.status(StatusCodes.NOT_FOUND).json({
+        success: false,
+        message: `Route with id [${id}] not found`,
+      });
+    }
+    res.status(StatusCodes.OK).json({
+      success: true,
+      message: "Route updated successfully",
+      data: updatedRoute,
+    });
+  } catch (error) {
+    next(error);
+  }
+};
 
-export const routeDelete = (req, res) => {
-    const route = deleteRoute(req.params.id);
-    
-    res.send(route);
-}
+export const routeDelete = async (req, res, next) => {
+  const { id } = req.params;
+  try {
+    const deletedRoute = await deleteRoute(id);
+    if (!deletedRoute) {
+      return res.status(StatusCodes.NOT_FOUND).json({
+        success: false,
+        message: `Route with id [${id}] not found`,
+      });
+    }
+    res.status(StatusCodes.OK).json({
+      success: true,
+      message: "Route deleted successfully",
+      data: deletedRoute,
+    });
+  } catch (error) {
+    next(error);
+  }
+};
